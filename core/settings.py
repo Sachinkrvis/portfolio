e5ead7bf1da9f5a535a480ea2d8b91d2f990a42b
Sachinkrvis <Sachinkrvis160@gmail.com>
2025-09-06 12:36:52 +0000

    update the .env

--- core/settings.py
+++ core/settings.py
@@ -152,5 +152,5 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "sachinkrvis160@gmail.com"
-EMAIL_HOST_PASSWORD = "qhjuedvzbrkgjdxc"  # not your real password, use App Password
+EMAIL_HOST_PASSWORD = os.getenv('GMAIL_URL') 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
